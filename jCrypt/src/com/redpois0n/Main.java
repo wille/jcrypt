93f22bc20083f3c5b476ed1bd47f9c2ff88ce93a
redpois0n <git@redpois0n>
2013-06-07 11:22:46 +0200

    1.3>1.4

--- jCrypt/src/com/redpois0n/Main.java
+++ jCrypt/src/com/redpois0n/Main.java
@@ -5,7 +5,7 @@ import javax.swing.UIManager;
 public class Main {
 	
 	public static final String getVersion() {
-		return "1.3";
+		return "1.4";
 	}
 
 	public static void main(String[] args) throws Exception {
