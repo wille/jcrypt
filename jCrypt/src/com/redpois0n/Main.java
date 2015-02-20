997a81df0ad025ad6a646fd828687c65941e1d18
redpois0n <git@redpois0n>
2015-02-20 23:20:22 +0100

    Version 1.5

--- jCrypt/src/com/redpois0n/Main.java
+++ jCrypt/src/com/redpois0n/Main.java
@@ -5,7 +5,7 @@ import javax.swing.UIManager;
 public class Main {
 	
 	public static final String getVersion() {
-		return "1.4";
+		return "1.5";
 	}
 
 	public static void main(String[] args) throws Exception {
